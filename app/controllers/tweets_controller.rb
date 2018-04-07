194b4010e3dfac6573243520687dcb3d5aa6ab2f
Yuji Yamamoto <bew1cwin@tky2.3web.ne.jp>
2018-04-07 19:22:27 +0000

    init

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,57 @@
+class TweetsController < ApplicationController
+  before_action :set_tweet, only: [:show, :edit, :update, :destroy]
+  
+  def index
+    @tweets = Tweet.order(:created_at)
+  end
+  
+  def new
+    if params[:back]
+      @tweet = Tweet.new(tweet_params)
+    else
+      @tweet = Tweet.new
+    end
+  end
+  
+  def confirm
+    @tweet = Tweet.new(tweet_params)
+    render 'new' if @tweet.invalid?
+  end
+  
+  def create
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      redirect_to tweets_path, notice: "つぶやきを作成しました！"
+    else
+      render 'new'
+    end
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @tweet.update(tweet_params)
+      redirect_to tweets_path, notice: "つぶやきを編集しました！"
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @tweet.destroy
+    redirect_to tweets_path, notice: "つぶやきを削除しました！"
+  end
+  
+  private
+  def tweet_params
+    params.require(:tweet).permit(:content)
+  end
+  
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  root to: 'tweets#index'
+  
+  resources :tweets do
+    collection do
+      post :confirm
+    end
+  end
+  
+end
